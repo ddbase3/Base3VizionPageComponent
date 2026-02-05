5d4f01259a7b8b0e4946bb791ac64c76ab233dea
qualitus-dahme <Dahme@qualitus.de>
2026-02-05 20:16:49 +0100

    lang vars added

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = 'Base3VizionPC';
 $version = '4.1.0';
 $ilias_min_version = '10.0';
-$ilias_max_version = '10.999';
+$ilias_max_version = '11.999';
 $responsible = 'Daniel Dahme';
 $responsible_mail = 'info@base3.de';
 
